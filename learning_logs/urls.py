a60c5291d20859152a3c6d2bcdf7882cb8df0824
Jalalgorithm <temitomzi@gmail.com>
2024-11-22 19:44:47 +0100

    Implementation of Topic details page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,5 +8,13 @@ app_name = 'learning_logs'
 
 urlpatterns=[
     #Home page
-    path('' , views.index , name='index')
+    path('' , views.index , name='index'),
+    
+    #Topics Page 
+    path('topics/' , views.topics , name='topics'),
+    
+    #Details page for a single topic
+    path('topics/<int:topic_id>/' , views.topic , name='topic')
+    
+    
 ]
