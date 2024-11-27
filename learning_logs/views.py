4a483c091ec9ae1f21b29754fe79c7fd9997383c
Jalalgorithm <temitomzi@gmail.com>
2024-11-27 02:29:29 +0100

    Added user authentication

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic , Entry
 
@@ -12,7 +13,7 @@ def index(request):
 
 @login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics':topics}
     
     return render(request, 'learning_logs/topics.html', context)
@@ -22,6 +23,10 @@ def topics(request):
 @login_required
 def topic(request , topic_id):
     topic = Topic.objects.get(id=topic_id)
+    
+    if topic.owner!=request.user:
+        raise Http404
+    
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic , 'entries':entries}
     
@@ -36,8 +41,11 @@ def new_topic(request):
     else:
         form = TopicForm(data=request.POST)
         if form.is_valid:
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
+        
         
     context={'form': form}
     return render(request , 'learning_logs/new_topic.html' , context)
@@ -68,6 +76,9 @@ def new_entry(request , topic_id):
 def edit_entry(request , entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    
+    if topic.owner !=request.user:
+        raise Http404
     
     if request.method!='POST':
         form = EntryForm(instance = entry)
